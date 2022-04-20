676b5ceaed435ad735c79e3fd62361e67ef2d5f7
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-04-20 23:34:14 +0200

    Add publish conf

---
+++ publish-conf.scala
@@ -0,0 +1,4 @@
+//> using publish.user "env:PUBLISH_USER"
+//> using publish.password "env:PUBLISH_PASSWORD"
+//> using publish.secretKey "env:PUBLISH_SECRET_KEY"
+//> using publish.secretKeyPassword "env:PUBLISH_SECRET_KEY_PASSWORD"
