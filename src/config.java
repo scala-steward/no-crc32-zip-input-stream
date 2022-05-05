dc2d61637278f5c3c065f5755c4d5e734f613d43
Alexandre Archambault <alexandre.archambault@gmail.com>
2022-05-05 15:49:58 +0200

    Initial commit

---
+++ src/config.java
@@ -0,0 +1,14 @@
+//> using publish.organization "io.github.alexarchambault.scala-cli.tmp"
+//> using publish.moduleName "zip-input-stream"
+//> using publish.computeVersion "git:tag"
+
+//> using publish.repository "central-s01"
+//> using publish.user "env:PUBLISH_USER"
+//> using publish.password "env:PUBLISH_PASSWORD"
+//> using publish.secretKey "env:PUBLISH_SECRET_KEY"
+//> using publish.secretKeyPassword "env:PUBLISH_SECRET_KEY_PASSWORD"
+
+//> using publish.license "GPL-2.0-with-classpath-exception"
+//> using publish.url "https://github.com/scala-cli/no-crc32-zip-input-stream"
+//> using publish.versionControl "github:scala-cli/no-crc32-zip-input-stream"
+//> using publish.developer "alexarchambault|Alex Archambault|https://github.com/alexarchambault"
